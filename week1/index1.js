c916d87cf459f9b86365dd79b2e4809248d55408
Ujjawal Kumar <ujjawalkr67@gmail.com>
2024-06-15 12:27:15 +0530

    foundation

---
+++ week1/index1.js
@@ -0,0 +1,19 @@
+console.log("hwlle")
+var firstname="ujawal";
+var lastname="kumar"
+var greet=`goodmorning ${firstname} ${lastname}`
+console.log(greet)
+gender="female"
+if(gender="male")
+    {
+    console.log(`goodmorning ${firstname} ${lastname}`)
+    }
+    else{
+    console.log(`goodmorning female ${firstname} ${lastname}`)
+        
+    }
+
+for(i=0;i<=1000;i++)
+    {
+        console.log(i)
+    }
