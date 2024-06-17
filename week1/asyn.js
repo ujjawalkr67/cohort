a7cca67fdfbad7c8596e5166bd560d3df7b893b9
Ujjawal Kumar <ujjawalkr67@gmail.com>
2024-06-17 18:58:19 +0530

    promises

--- week1/asyn.js
+++ week1/asyn.js
@@ -6,24 +6,33 @@
 // console.log("hi there") //this will console first
 
 
-//promises
-const fs = require("fs");
+// //promises
+// const fs = require("fs");
 
-//my own asynchrounus function
-function ujjawalReadFiles(){
-    console.log("inside ujjawal file");
-    return new Promise(function (resolve){
-        console.log("inside promise")
-        fs.readFile("a.txt","utf-8",function(err,data){
-            console.log("before resolve")    
-            resolve(data)
-    });
-    })
-}
+// //my own asynchrounus function
+// function ujjawalReadFiles(){
+//     console.log("inside ujjawal file");
+//     return new Promise(function (resolve){
+//         console.log("inside promise")
+//         fs.readFile("a.txt","utf-8",function(err,data){
+//             console.log("before resolve")    
+//             resolve(data)
+//     });
+//     })
+// }
 
-//callback function to call
-function onDone(data){
-    console.log(data)
-}
+// //callback function to call
+// function onDone(data){
+//     console.log(data)
+// }
 
-ujjawalReadFiles().then(onDone)
+// ujjawalReadFiles().then(onDone)
+
+const GITHUB_API ="https://api.github.com/users/ujjawalkr67"
+const user = fetch(GITHUB_API) //fetch is a type of promise
+console.log(user)
+ //attachong callback to promise 
+// user.then(function(data){
+//     console.log(data)
+// })
+
