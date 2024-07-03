7dce41a522e383a462d84974884866309b11427e
Ujjawal Kumar <ujjawalkr67@gmail.com>
2024-07-03 19:31:05 +0530

    express

--- week2/index.js
+++ week2/index.js
@@ -4,12 +4,25 @@ const app=express();
 const port = 3000;
 //This is basic expree code
 //basic route
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+// //   res.send('Hello World!')
+//   res.send('<b>Hello there!<b>') //html tag
+
+// })
+
 //another route if we go to /ujju
-app.get('/ujju', (req, res) => {
-    res.send('Hello ujjawaal!')
+app.post('/ujju', (req, res) => {
+    res.send('<b>Hello ujjawaal!<b>')
+  })
+
+  app.post('/uu', (req, res) => {
+    console.log(req.body) //this will give undefined;
+    console.log(req.headers)
+    res.send({
+        msg:"2=2=4"
+    })
   })
 //go to locahost 3000
 app.listen(port)
+
+// ------------------------------------------Create a to do app that lets users store on the server-----------------------
