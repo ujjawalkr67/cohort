45ccfd14126c475e79ed9e34bd27cebaa04e1859
Ujjawal Kumar <ujjawalkr67@gmail.com>
2024-07-22 01:18:52 +0530

    database

---
+++ week3/autentication.js
@@ -0,0 +1,98 @@
+
+//lets create a website which has two end points-
+// post/signin
+// body{
+//     userername:string
+//     password:string
+// }
+// return json web token with username encrypted
+// get/user-
+// Authorizaton Headers
+// return an array of all user if user is registered in (token is correct)
+// return 403 satus code
+
+const express= require("express");
+const jwt=require("jsonwebtoken");
+const jwtPassword='123456';
+
+const app=express();
+app.use(express.json())
+
+const ALL_USERS=[
+    {
+        username:"ujjawalkr67@gmail.com",
+        password:"12123434",
+        name:'Ujjawal'
+    },
+    {
+        username:"maddy@gmail.com",
+        password:"1234",
+        name:'Maddy'
+    },
+    {
+        username:"samy@gmail.com",
+        password:"092309",
+        name:'Sam'
+    },
+];
+
+function userExists(username,password)
+{
+    //logic to return true or false if this user exist or not
+    let userExists=false;
+    for(let i=0;i<ALL_USERS.length;i++)
+    {
+        if(ALL_USERS[i].username==username && ALL_USERS[i].password==password)
+        {
+            userExists= true;
+        }
+    }
+    return userExists;
+}
+
+app.post('/signin',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+    if(!userExists(username,password)){
+        return res.status(403).json({
+            msg:"user does not exits in memory"
+        });
+    }
+    var token = jwt.sign({username:username}, jwtPassword);
+    return res.json({
+        token,
+    });
+});
+
+
+
+
+app.get('/users',function(req,res){
+    const token= req.headers.authorization;
+    try{
+        const decoded=jwt.verify(token,jwtPassword);
+        const username=decoded.username;
+        //return the list pf other than this username
+        res.json({
+            // users:ALL_USERS
+            users:ALL_USERS.filter(function(value){
+                if(value.username==username)
+                {
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            })
+        })
+
+    }
+    catch(err)
+    {
+        return  res.status(403).json({
+            msg:"Invalid token"
+        })
+    }
+});
+
+app.listen(3000);
---
+++ week3/basic_data-base.js
@@ -0,0 +1,43 @@
+const express= require("express");
+const jwt=require("jsonwebtoken");
+const jwtPassword='123456';
+
+const app=express();
+app.use(express.json())
+
+const mongoose=require("mongoose")
+
+mongoose.connect('mongodb+srv://admin:EWetFQKhuQl6cmdo@cluster0.znad7af.mongodb.net/')
+
+const User = mongoose.model('Users', { name: String ,email: String,password:String});
+
+// const newuser = new User({ name:'Ujjawal',username:"ujjawalkr67@gmail.com",
+//     password:"12123434" });
+
+// newuser.save()
+
+//now add the same way  or a post request
+
+app.post('/signin', async function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+    const name=req.body.name;
+
+    const existinguser=await User.findOne({email:username});
+    if(existinguser){
+        return res.status(400).send("Username already exist")
+    }
+const newuser = new User({ 
+    name:name,
+    username:username,
+    password:password
+});
+
+newuser.save()
+
+    return res.json({
+        msg:"succefuuly sign in  "
+    });
+});
+
+app.listen(3000);
